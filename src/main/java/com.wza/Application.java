de78ee65c9a2de70a3e9eb8f0621e840bb673032
WU Zi Ang <ziang.wu@ufinity.com>
2017-05-13 18:45:41 +0800

    convert spring boot jar application to a WAR

--- src/main/java/com.wza/Application.java
+++ src/main/java/com.wza/Application.java
@@ -1,28 +1,19 @@
 package com.wza;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.system.ApplicationPidListener;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan
-@PropertySource("classpath:application.properties")
-public class Application {
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
 
-  public static void main(String[] args) {
-    SpringApplication springApplication =
-            new SpringApplication(Application.class);
-    springApplication.addListeners(
-            new ApplicationPidListener("/wzasg/target/app.pid"));
-    springApplication.run(args);
-    System.out.println("Spring boot started");
-
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(Application.class);
   }
 
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
 }
